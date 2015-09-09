850ca2a1902c7301c61f3c573d08da3cab0a6272
jackpower <jack@objectivemoney.co.za>
2015-09-09 09:14:06 +0200

    Allow language other in planner profile and change DWQA image size

--- src/resources/wp-content/themes/dazzling-child/functions.php
+++ src/resources/wp-content/themes/dazzling-child/functions.php
@@ -7,3 +7,19 @@ function hide_update_notice_to_all_but_admin_users()
     }
 }
 add_action( 'admin_head', 'hide_update_notice_to_all_but_admin_users', 1 );
+
+add_action( 'admin_init', 'my_remove_menu_pages' );
+ 
+function my_remove_menu_pages() {
+	// If the user does not have access to publish posts
+	if(!current_user_can('publish_posts')) {
+		// Remove the "Tools" menu
+		remove_menu_page('index.php');
+	}
+}
+
+add_action("user_register", "set_user_admin_bar_false_by_default", 10, 1);
+function set_user_admin_bar_false_by_default($user_id) {
+    update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
+    update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
+}
