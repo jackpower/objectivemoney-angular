ae145763a26d2644927ea61cf6dc38b9dd3383e8
jackpower <jack@objectivemoney.co.za>
2015-09-09 11:05:53 +0200

    Include login logout on WP

--- dist/resources/wp-content/themes/dazzling-child/functions.php
+++ dist/resources/wp-content/themes/dazzling-child/functions.php
@@ -23,3 +23,18 @@ function set_user_admin_bar_false_by_default($user_id) {
     update_user_meta( $user_id, 'show_admin_bar_front', 'false' );
     update_user_meta( $user_id, 'show_admin_bar_admin', 'false' );
 }
+
+//Add login/logout link to naviagation menu
+function add_login_out_item_to_menu( $items, $args ){
+
+	//change theme location with your them location name
+	if( is_admin() ||  $args->theme_location != 'primary' )
+		return $items; 
+
+	$redirect = ( is_home() ) ? false : get_permalink();
+	if( is_user_logged_in( ) )
+		$link = '<a href="' . wp_logout_url( $redirect ) . '" title="' .  __( 'Logout' ) .'">' . __( 'Logout' ) . '</a>';
+	else  $link = '<a href="' . wp_login_url( $redirect  ) . '" title="' .  __( 'Login' ) .'">' . __( 'Login' ) . '</a>';
+
+	return $items.= '<li id="log-in-out-link" class="menu-item menu-type-link">'. $link . '</li>';
+}add_filter( 'wp_nav_menu_items', 'add_login_out_item_to_menu', 50, 2 );
